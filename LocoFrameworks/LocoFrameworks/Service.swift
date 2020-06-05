f87a7dffc54d11446b23f71f7adb884d426cb21b
Krishna Mohan <krishnamohan@authenticid.co>
2020-06-05 19:21:16 +0530

    first commit

---
+++ LocoFrameworks/LocoFrameworks/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  LocoFrameworks
+//
+//  Created by AID011 on 05/06/20.
+//  Copyright © 2020 AID011. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init  () {}
+    
+    public static func dosomething() -> String{
+        return "Krishna Mohan"
+    }
+}
